6dea62616328b5256834c4dfe2809a218aa90fff
lesand <leonelsd7@gmail.com>
2014-03-29 17:35:16 -0600

    adding long press appxilio

--- AndroidApp/Appxilio/gen/com/example/appxilio/R.java
+++ AndroidApp/Appxilio/gen/com/example/appxilio/R.java
@@ -993,10 +993,10 @@ containing a value of this type.
         public static final int police=0x7f020060;
     }
     public static final class id {
-        public static final int ImageAmbulance=0x7f050040;
-        public static final int ImageFireFighter=0x7f05003f;
-        public static final int ImageHistory=0x7f05003e;
-        public static final int ImageStatus=0x7f050042;
+        public static final int ImageAmbulance=0x7f05003f;
+        public static final int ImageFireFighter=0x7f05003e;
+        public static final int ImageHistory=0x7f05003d;
+        public static final int ImageStatus=0x7f050041;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1008,13 +1008,12 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050044;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
-        public static final int container=0x7f05003c;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
@@ -1028,9 +1027,9 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int imagePolice=0x7f050041;
-        public static final int imageView1=0x7f05003d;
-        public static final int imageView2=0x7f050043;
+        public static final int imagePolice=0x7f050040;
+        public static final int imageView1=0x7f05003c;
+        public static final int imageView2=0x7f050042;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
--- AndroidApp/Appxilio/src/com/example/appxilio/MainActivity.java
+++ AndroidApp/Appxilio/src/com/example/appxilio/MainActivity.java
@@ -3,34 +3,93 @@ package com.example.appxilio;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
+	public static int LONG_PRESS_TIME = 500; // Time in miliseconds
+	boolean isSpeakButtonLongPressed = false;
+
+	  final Handler _handler = new Handler(); 
+	  Runnable _longPressed = new Runnable() { 
+	      public void run() {
+	          Toast.makeText(getApplicationContext(), "Long press", Toast.LENGTH_SHORT).show();
+	      }   
+	  };
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		ImageView police = (ImageView)findViewById(R.id.imagePolice);
+		
+		police.setOnClickListener( new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+			 	
+				Toast.makeText(getApplicationContext(), "damn", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		police.setOnLongClickListener(new OnLongClickListener() {
+			
+			@Override
+			public boolean onLongClick(View v) {
+				 isSpeakButtonLongPressed = true;
+				 Toast.makeText(getApplicationContext(), "press begin", Toast.LENGTH_SHORT).show();
+	             return true;
+			}
+		});
+		
+		police.setOnTouchListener(speakTouchListener);
 	}
+	
+	 private View.OnTouchListener speakTouchListener = new View.OnTouchListener()
+	 {
+		 @Override
+         public boolean onTouch(View pView, MotionEvent pEvent) {
+              pView.onTouchEvent(pEvent);
+              // We're only interested in when the button is released.
+              if (pEvent.getAction() == MotionEvent.ACTION_UP) {
+                   // We're only interested in anything if our speak button is currently pressed.
+                   if (isSpeakButtonLongPressed) {
+                	   Toast.makeText(getApplicationContext(), "press end", Toast.LENGTH_SHORT).show();
+                        isSpeakButtonLongPressed = false;
+                   }
+              }
+              return false;
+         }
+     
+	 };
+	 
+	 
+
+        
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
-		// Inflate the menu; this adds items to the action bar if it is present.
+		
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
