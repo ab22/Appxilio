6f00a082183e221f1b0e8d8f7162161e817722c2
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2014-03-29 16:02:24 -0600

    Added node files

---
+++ Dashboard/app.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+    exphbs  = require('express3-handlebars'),
+
+    app = express();
+
+app.configure(function(){
+    app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
+    app.use(express.static(__dirname + '/public'));
+});
+
+
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/heatmap', function(req, res) {
+    res.render('heatmap.handlebars');
+});
+
+app.listen(3000);
